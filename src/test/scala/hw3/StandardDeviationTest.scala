e86bb460647e8fe300279e3b8b2395a7c79d8a5f
otakar vinklar <ota.vinklar@gmail.com>
2020-01-14 22:05:11 +0100

    standard deviation implementation

--- src/test/scala/hw3/StandardDeviationTest.scala
+++ src/test/scala/hw3/StandardDeviationTest.scala
@@ -4,23 +4,38 @@ import hw3.Main.standardDeviation
 import org.scalatest.{FunSuite, Matchers}
 
 class StandardDeviationTest extends FunSuite with Matchers {
-  test("stddev example")     {
+  test("stddev example") {
+
     /**
-     * x	                 4      9    11   12    17     5     8    12    14
-     * (x - avg(x))^2   38.7  1.49  0.60  3.16  45.9  27.3  4.94  3.16  14.3
-     *
-     * sum:    sum(x) = 139.55
-     * avg:    139.55/9 = 15.51
-     * stddev: sqrt(15.51) = 3.94
-     */
+      * x	                 4      9    11   12    17     5     8    12    14
+      * (x - avg(x))^2   38.7  1.49  0.60  3.16  45.9  27.3  4.94  3.16  14.3
+      *
+      * sum:    sum(x) = 139.55
+      * avg:    139.55/9 = 15.51
+      * stddev: sqrt(15.51) = 3.94
+      */
     standardDeviation(List(4, 9, 11, 12, 17, 5, 8, 12, 14)) shouldBe 3.93 +- 0.01
   }
-  test("stddev singleton example")     {
+  test("stddev singleton example") {
+
     /**
-     * x	                  42
-     * (x - avg(x))^2        0
-     * stddev: sqrt(0) = 0
-     */
+      * x	                  42
+      * (x - avg(x))^2        0
+      * stddev: sqrt(0) = 0
+      */
     standardDeviation(List(42)) shouldBe 0.0
   }
+
+  test("empty list") {
+
+    /**
+      * x	                  42
+      * (x - avg(x))^2        0
+      * stddev: sqrt(0) = 0
+      */
+    intercept[AssertionError] {
+      standardDeviation(List()) shouldBe 0.0
+    }
+  }
+
 }
